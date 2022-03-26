0355ea0d5327d0f3b1dde1bb9b437c5b25cd2acc
Luca Zani <lucazani2002@gmail.com>
2022-03-26 14:30:12 +0100

    style: added theme.ts for theming

---
+++ src/utils/theme.ts
@@ -0,0 +1,16 @@
+const colors = {
+  purpleLight: "#6E49CB",
+  purpleDark: "#380D75",
+  orange300: "#FCA121",
+  orange600: "#FC6D26",
+  orange900: "#E24329",
+  gray300: "#CCCCCC",
+  gray600: "#8C929D",
+  gray900: "#2E2E2E",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
