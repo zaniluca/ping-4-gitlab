0b17eea17eb548e5e973680e7ec20091143bc210
Luca Zani <lucazani2002@gmail.com>
2022-03-29 22:37:08 +0200

    fix: added goback functionality to back btn

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -2,10 +2,24 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import theme from "../utils/theme";
 import { ArrowLeft } from "react-native-feather";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/types";
 
 const BackButton = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const handleGoBack = () => {
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    } else {
+      console.log("Can't go back");
+    }
+  };
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={handleGoBack}>
       <ArrowLeft stroke={theme.colors.gray900} />
       <Text style={styles.text}>Back</Text>
     </TouchableOpacity>
