c1a3214bfe2a654b1b8985ff414c7c3e5b4287d1
Luca <lucazani2002@gmail.com>
2022-04-08 23:09:35 +0200

    feat: bootstrapped firebase and migrated to devclients

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import "expo-dev-client";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
