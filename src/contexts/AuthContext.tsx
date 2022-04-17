5bd25ea140a309da0a89163bd9722a6f13f0eef1
Luca Zani <lucazani2002@gmail.com>
2022-04-17 14:23:10 +0200

    feat(firebase auth): added login, signup and signout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,8 +6,13 @@ import {
   useState,
 } from "react";
 import {
+  createUserWithEmailAndPassword,
+  EmailAuthProvider,
+  linkWithCredential,
   onAuthStateChanged,
   signInAnonymously as anonymousSignIn,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { User } from "../utils/types";
@@ -15,13 +20,19 @@ import { User } from "../utils/types";
 type AuthContextValues = {
   user: User;
   loading: boolean;
-  signInAnonymously: VoidFunction;
+  signInAnonymously: () => Promise<void>;
+  signup: (e: string, p: string) => Promise<void>;
+  login: (e: string, p: string) => Promise<void>;
+  logout: () => Promise<void>;
 };
 
 export const AuthContext = createContext<AuthContextValues>({
   user: null,
   loading: true,
-  signInAnonymously: () => {},
+  signInAnonymously: async () => {},
+  signup: async () => {},
+  login: async () => {},
+  logout: async () => {},
 });
 
 type AuthContextProps = {
@@ -44,19 +55,57 @@ export const AuthProvider: React.FC<AuthContextProps> = ({ children }) => {
   const signInAnonymously = async () => {
     try {
       const user = await anonymousSignIn(auth);
-
       console.log("Logged in with uid: ", user.user.uid);
     } catch (error) {
       if (error.code === "auth/operation-not-allowed") {
         console.log("Enable anonymous in your firebase console.");
       }
+      console.error(error);
+    }
+  };
 
+  const signup = async (e: string, p: string) => {
+    if (user) {
+      // Anonymous user trying to get a permanent account
+      try {
+        const credential = EmailAuthProvider.credential(e, p);
+        const result = await linkWithCredential(user, credential);
+        console.log("Anonymous user upgraded to permanent: ", result.user.uid);
+      } catch (error) {
+        console.error("Error trying to upgrade anonymous user: ", error);
+      }
+    } else {
+      // New user
+      try {
+        const result = await createUserWithEmailAndPassword(auth, e, p);
+        console.log("Signed up with uid: ", result.user.uid);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const login = async (e: string, p: string) => {
+    try {
+      const result = await signInWithEmailAndPassword(auth, e, p);
+      console.log("Logged in with uid: ", result.user.uid);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <AuthContext.Provider value={{ user, loading, signInAnonymously }}>
+    <AuthContext.Provider
+      value={{ user, loading, signInAnonymously, signup, login, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
