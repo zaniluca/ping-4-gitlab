54e791d34c0cd2edd6777d53b3f309bcd1af3eda
Luca Zani <lucazani2002@gmail.com>
2022-04-19 23:14:28 +0200

    feat(toasts): added onpress functionality

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -8,6 +8,7 @@ import { Theme } from "../utils/theme";
 import { Box, Text } from "./restyle";
 import { CheckCircle, Info, CloudLightning } from "react-native-feather";
 import { SvgProps } from "react-native-svg";
+import { TouchableOpacity } from "react-native";
 
 type CustomToastParams = {
   Icon?: (props: SvgProps) => JSX.Element;
@@ -20,35 +21,38 @@ type CustomToastProps = ToastConfigParams<BaseToastProps> &
 const BaseToast: React.FC<CustomToastProps> = ({
   text1,
   text2,
+  onPress,
   backgroundColor,
   Icon,
 }) => (
-  <Box
-    backgroundColor={backgroundColor}
-    paddingHorizontal="l"
-    paddingVertical="m"
-    borderRadius={4}
-    flexDirection="row"
-    alignItems="center"
-    minHeight={48}
-    width="95%"
-  >
-    {Icon && (
-      <Box paddingRight="m">
-        <Icon stroke="white" />
-      </Box>
-    )}
-    <Box flexDirection="column" flexShrink={1}>
-      <Text variant="headline" color="white">
-        {text1}
-      </Text>
-      {text2 && (
-        <Text marginTop="xxs" variant="callout" color="white">
-          {text2}
-        </Text>
+  <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
+    <Box
+      backgroundColor={backgroundColor}
+      paddingHorizontal="l"
+      paddingVertical="m"
+      borderRadius={4}
+      flexDirection="row"
+      alignItems="center"
+      minHeight={48}
+      width="95%"
+    >
+      {Icon && (
+        <Box paddingRight="m">
+          <Icon stroke="white" />
+        </Box>
       )}
+      <Box flexDirection="column" flexShrink={1}>
+        <Text variant="headline" color="white">
+          {text1}
+        </Text>
+        {text2 && (
+          <Text marginTop="xxs" variant="callout" color="white">
+            {text2}
+          </Text>
+        )}
+      </Box>
     </Box>
-  </Box>
+  </TouchableOpacity>
 );
 
 const toastConfig: ToastConfig = {
