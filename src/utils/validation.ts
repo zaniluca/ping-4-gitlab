c534a72881d5ff46e65ba7e6d4d23aa1a8fe1678
Luca Zani <lucazani2002@gmail.com>
2022-04-25 13:29:32 +0200

    feat(validation): added more validation to signup

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,14 +1,17 @@
 import * as Yup from "yup";
 
-const emailBasicValidation = Yup.string()
-  .email("Email must be a valid email")
-  .required("Email is required");
+const emailBasicValidation = Yup.string().email().required().label("Email");
 
-const passwordBasicValidation = Yup.string().required("Password is required");
+const passwordBasicValidation = Yup.string().required().label("Password");
 
 export const SignupSchema = Yup.object({
   email: emailBasicValidation,
-  password: passwordBasicValidation,
+  password: passwordBasicValidation
+    .min(6)
+    .matches(/^(?=.*[a-z])/, "Must contain at least one lowercase character")
+    .matches(/^(?=.*[A-Z])/, "Must contain at least one uppercase character")
+    .matches(/^(?=.*[0-9])/, "Must contain at least one number")
+    .matches(/^(?=.*[!@#%&])/, "Must contain at least one special character"),
   confirmPassword: Yup.string()
     .required("Please confirm your password")
     .oneOf([Yup.ref("password"), null], "Passwords must match"),
