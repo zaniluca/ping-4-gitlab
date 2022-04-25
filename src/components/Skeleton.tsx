ebc742bb0cd864533217e51505e6bc909b1f477f
Luca Zani <lucazani2002@gmail.com>
2022-04-25 19:13:39 +0200

    feat: added skeleton component

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,43 @@
+import { Animated } from "react-native";
+import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { BoxProps } from "@shopify/restyle";
+import { Theme } from "../utils/theme";
+import { Box } from "./restyle";
+
+type Props = BoxProps<Theme> & {
+  children?: JSX.Element;
+};
+
+const Skeleton: React.FC<Props> = ({ children, ...rest }) => {
+  const opacity = useRef(new Animated.Value(0.3));
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(opacity.current, {
+          toValue: 1,
+          useNativeDriver: true,
+          duration: 500,
+        }),
+        Animated.timing(opacity.current, {
+          toValue: 0.3,
+          useNativeDriver: true,
+          duration: 800,
+        }),
+      ])
+    ).start();
+  }, [opacity]);
+
+  return (
+    <Animated.View style={{ opacity: opacity.current, flex: 1 }}>
+      {/* Defaulting this background color, can be overrided by props */}
+      <Box backgroundColor="gray600" {...rest}>
+        {children}
+      </Box>
+    </Animated.View>
+  );
+};
+
+export default Skeleton;
