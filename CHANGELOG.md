50a7ecf068c1fd74878b20a70e15f230e7c9d865
Luca Zani <lucazani2002@gmail.com>
2022-05-25 22:22:25 +0200

    enhancements to auth forms (#79)
    * refactor: optimized input in auth forms
    
    * feat: added password rules for ios in signup form
    
    * refactor: moved errors list to component
    
    * refactor: moved errors list to component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,8 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [unreleased]
 
-### Changes
+## Changes
 
+- Improvements to auth forms
 - Only load 50 notifications in inbox
 
 ### Fixed
---
+++ src/components/ErrorsList.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, Text } from "./restyle";
+
+type Props = {
+  errors: Record<string, string>;
+};
+
+const ErrorsList: React.FC<Props> = ({ errors }) => {
+  return (
+    <Box>
+      <Text color="red">Some errors occurred:</Text>
+      {Object.entries(errors).map(([key, value]) => (
+        <Box key={key} flexDirection="row">
+          <Text color="red">{"\u2022"}</Text>
+          <Text color="red" paddingLeft="xs">
+            {value}
+          </Text>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default ErrorsList;
