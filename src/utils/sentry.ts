a6953c9cf0e41e43e7bb0d6e7728b0698e5eb7ee
Luca Zani <lucazani2002@gmail.com>
2022-07-27 23:43:48 +0200

    fix(sentry): transactions not being recorded (#110)

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,3 +1,4 @@
+import Constants from "expo-constants";
 import * as Sentry from "sentry-expo";
 
 Sentry.init({
@@ -7,4 +8,6 @@ Sentry.init({
   enableAutoSessionTracking: true,
   // Sessions close after app is 10 seconds in the background.
   sessionTrackingIntervalMillis: 10000,
+  tracesSampleRate: 0.75,
+  release: Constants.manifest?.revisionId,
 });
