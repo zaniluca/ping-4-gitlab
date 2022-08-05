f42c43c874939b93e535e6b0ec02d662272eaaa6
Luca Zani <lucazani2002@gmail.com>
2022-08-05 12:30:26 +0200

    fix: another fix to the development api url

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,14 +4,21 @@ import { Platform } from "react-native";
 
 import { getAccessTokenFromSecureStore } from "./jwt";
 
-const host =
-  Platform.OS === "android" && __DEV__
-    ? // 10.0.2.2 is the special host for accessing localhost via android emulator
-      "10.0.2.2"
-    : // Getting the IP address of the device we're developing on to make work with Expo GO
-      Constants.manifest?.debuggerHost!.split(":").shift();
+const getDevelopmentApiUrl = () => {
+  const host =
+    Platform.OS === "android"
+      ? // 10.0.2.2 is the special host for accessing localhost via android emulator
+        "10.0.2.2"
+      : // Getting the IP address of the device we're developing on to make work with Expo GO
+        Constants.manifest?.debuggerHost?.split(":").shift();
 
-const API_URL = process.env.API_URL ?? `http://${host}:8080`;
+  return `http://${host}:8080`;
+};
+
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.API_URL
+    : getDevelopmentApiUrl();
 
 export const http = axios.create({
   baseURL: API_URL,
