ae87420d30d28713480ec684aa93ffb35d363456
Luca Zani <lucazani2002@gmail.com>
2022-09-18 13:23:26 +0200

    fix(updates): trying to fix ota updates not working

--- src/hooks/use-updates.ts
+++ src/hooks/use-updates.ts
@@ -1,19 +1,17 @@
 import * as Updates from "expo-updates";
 import { useEffect, useState } from "react";
 import Toast from "react-native-toast-message";
+import * as Sentry from "sentry-expo";
 
 export const useUpdates = () => {
   const [isCheckingForUpdate, setIsCheckingForUpdate] = useState(false);
 
-  useEffect(() => {
-    // We cannot check for updates in development
-    // Or the app will crash
-    if (__DEV__) return;
-
+  const handleCheckForUpdate = async () => {
     console.log("Checking for updates...");
-    setIsCheckingForUpdate(true);
+    try {
+      setIsCheckingForUpdate(true);
+      const update = await Updates.checkForUpdateAsync();
 
-    Updates.checkForUpdateAsync().then((update) => {
       if (update.isAvailable) {
         console.log("New update available! Fetching...");
         Toast.show({
@@ -22,16 +20,46 @@ export const useUpdates = () => {
           text2: "Please wait while we download it.",
         });
 
-        Updates.fetchUpdateAsync().then(() => {
-          setIsCheckingForUpdate(false);
-
-          Updates.reloadAsync();
-        });
+        await Updates.fetchUpdateAsync();
+        setIsCheckingForUpdate(false);
+        await Updates.reloadAsync();
       } else {
         console.log("No updates available.");
-        setIsCheckingForUpdate(false);
       }
-    });
+    } catch (error) {
+      Sentry.Native.captureException(error);
+    }
+    setIsCheckingForUpdate(false);
+  };
+
+  useEffect(() => {
+    // We cannot check for updates in development
+    // Or the app will crash
+    if (__DEV__) return;
+    handleCheckForUpdate();
+
+    // console.log("Checking for updates...");
+    // setIsCheckingForUpdate(true);
+
+    // Updates.checkForUpdateAsync().then((update) => {
+    //   if (update.isAvailable) {
+    //     console.log("New update available! Fetching...");
+    //     Toast.show({
+    //       type: "info",
+    //       text1: "A new update is available!",
+    //       text2: "Please wait while we download it.",
+    //     });
+
+    //     Updates.fetchUpdateAsync().then(() => {
+    //       setIsCheckingForUpdate(false);
+
+    //       Updates.reloadAsync();
+    //     });
+    //   } else {
+    //     console.log("No updates available.");
+    //     setIsCheckingForUpdate(false);
+    //   }
+    // });
   }, []);
 
   return {
