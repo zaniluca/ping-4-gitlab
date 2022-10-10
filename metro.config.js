13c55f2fa46196e6279fc32501c5c00fad533db0
Luca Zani <lucazani2002@gmail.com>
2022-10-10 18:29:10 +0200

    chore: removed unused metro.config.js

--- metro.config.js
+++
@@ -1,5 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-// eslint-disable-next-line no-undef
-const defaultConfig = getDefaultConfig(__dirname);
-
-module.exports = defaultConfig;
