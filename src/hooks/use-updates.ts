db66c390278c025d56b1285883fa20ce50899259
Luca Zani <lucazani2002@gmail.com>
2024-06-13 21:11:18 +0200

    feat(sdk51): upgraded to sdk 51
    - Upgraded deps
    - Fixed
    - Fixed LinearGradient not being displayed properly

--- src/hooks/use-updates.ts
+++ src/hooks/use-updates.ts
@@ -1,59 +1,89 @@
 import * as Sentry from "@sentry/react-native";
 import * as Updates from "expo-updates";
-import { useState } from "react";
+import { useEffect } from "react";
 import Toast from "react-native-toast-message";
 
 export const useUpdates = () => {
-  const [isCheckingForUpdate, setIsCheckingForUpdate] = useState(false);
+  const {
+    isUpdateAvailable,
+    isChecking,
+    initializationError,
+    checkError,
+    downloadError,
+  } = Updates.useUpdates();
 
-  const updateEventHandler = async (event: Updates.UpdateEvent) => {
-    setIsCheckingForUpdate(true);
-
-    if (event.type === Updates.UpdateEventType.ERROR) {
-      console.error("Error while checking for updates", event.message);
-      Sentry.captureException(event.message);
-
-      setIsCheckingForUpdate(false);
-    } else if (event.type === Updates.UpdateEventType.NO_UPDATE_AVAILABLE) {
-      console.log("No updates available.");
-
-      setIsCheckingForUpdate(false);
-    } else if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
-      console.log("New update available! Fetching...");
-      Toast.show({
-        type: "info",
-        text1: "A new update is available!",
-        text2: "Please wait while we download it.",
-      });
-
-      const result = await Updates.fetchUpdateAsync();
-
-      console.log("Update fetched", result);
-
+  useEffect(() => {
+    if (!__DEV__) {
       Sentry.addBreadcrumb({
-        event_id: "fetch-update",
+        event_id: "check-for-update",
         category: "updates",
-        message: "Update fetched",
+        message: "Checking for updates",
         level: "info",
       });
+      Updates.checkForUpdateAsync();
+    }
+  }, []);
 
-      setIsCheckingForUpdate(false);
+  useEffect(() => {
+    if (initializationError) {
+      console.error(
+        "An error occurred during updates initialization",
+        initializationError
+      );
+      Sentry.captureException(initializationError);
+    } else if (checkError) {
+      console.error("An error occurred during updates check", checkError);
+      Sentry.captureException(checkError);
+    } else if (downloadError) {
+      console.error("An error occurred during updates download", downloadError);
+      Sentry.captureException(downloadError);
+      Toast.show({
+        type: "error",
+        text1: "An error occurred while downloading the update",
+        text2: "Please try again later.",
+      });
+    }
+  }, [initializationError, checkError, downloadError]);
 
-      if (result.isNew) {
-        console.log("New update available! Reloading...");
-        try {
-          await Updates.reloadAsync();
-        } catch (error) {
-          console.error("Could not reload app after update", error);
-          Sentry.captureException(error);
-        }
+  useEffect(() => {
+    if (isUpdateAvailable) {
+      update();
+    } else {
+      console.log("No new update available");
+    }
+  }, [isUpdateAvailable]);
+
+  const update = async () => {
+    console.log("New update available! Fetching...");
+    Toast.show({
+      type: "info",
+      text1: "A new update is available!",
+      text2: "Please wait while we download it.",
+    });
+
+    const result = await Updates.fetchUpdateAsync();
+
+    console.log("Update fetched", result);
+
+    Sentry.addBreadcrumb({
+      event_id: "fetch-update",
+      category: "updates",
+      message: "Update fetched",
+      level: "info",
+    });
+
+    if (result.isNew) {
+      console.log("New update available! Reloading...");
+      try {
+        await Updates.reloadAsync();
+      } catch (error) {
+        console.error("Could not reload app after update", error);
+        Sentry.captureException(error);
       }
     }
   };
 
-  Updates.useUpdateEvents(updateEventHandler);
-
   return {
-    isCheckingForUpdate,
+    isCheckingForUpdate: isChecking,
   };
 };
