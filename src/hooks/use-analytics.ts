f50c4e820b554a6cabb4b427af57319a8311c77e
Luca Zani <lucazani2002@gmail.com>
2025-02-01 14:07:16 +0100

    feat(posthog): user identification

--- src/hooks/use-analytics.ts
+++ src/hooks/use-analytics.ts
@@ -1,8 +1,25 @@
 import PostHog from "posthog-react-native";
 
-export const posthog = new PostHog(
-  process.env.EXPO_PUBLIC_POSTHOG_API_KEY ?? ""
-  // {
-  //   host: "https://eu.i.posthog.com",
-  // }
-);
+import { APIUser } from "../utils/types";
+
+const posthog = new PostHog(process.env.EXPO_PUBLIC_POSTHOG_API_KEY ?? "", {
+  host: "https://eu.i.posthog.com",
+});
+
+export const useAnalytics = () => {
+  const identify = (user: Partial<APIUser>) => {
+    posthog.identify(user.id, {
+      email: user.email,
+      hookId: user.hookId,
+    });
+  };
+
+  const reset = () => {
+    posthog.reset();
+  };
+
+  return {
+    identify,
+    reset,
+  };
+};
