d22d60b40d3bf23a970de3cfb6c030498d2565b7
Luca Zani <lucazani2002@gmail.com>
2026-05-25 23:08:43 +0200

    chore: trying to fix build memory

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,5 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
+import { withGradleProperties } from "@expo/config-plugins";
 
 const getIconForBuildEnv = () => {
   if (
@@ -19,25 +20,39 @@ type CustomConfig = Omit<ConfigContext, "config"> & {
     Required<Pick<ExpoConfig, "name" | "slug">>;
 };
 
-export default ({ config }: CustomConfig): ExpoConfig => ({
-  ...config,
-  ios: {
-    ...config.ios,
-    icon: getIconForBuildEnv(),
-  },
-  plugins: [
-    ...(config.plugins || []),
-    [
-      "@sentry/react-native/expo",
-      {
-        organization: process.env.SENTRY_ORG,
-        project: process.env.SENTRY_PROJECT,
-      },
-    ],
-  ],
-  extra: {
-    eas: {
-      projectId: "bdcab203-d9ce-48aa-95db-938dbfe1be17",
+export default ({ config }: CustomConfig): ExpoConfig => {
+  const expoConfig: ExpoConfig = {
+    ...config,
+    ios: {
+      ...config.ios,
+      icon: getIconForBuildEnv(),
     },
-  },
-});
+    plugins: [
+      ...(config.plugins || []),
+      [
+        "@sentry/react-native/expo",
+        {
+          organization: process.env.SENTRY_ORG,
+          project: process.env.SENTRY_PROJECT,
+        },
+      ],
+    ],
+    extra: {
+      eas: {
+        projectId: "bdcab203-d9ce-48aa-95db-938dbfe1be17",
+      },
+    },
+  };
+
+  return withGradleProperties(expoConfig, (props) => {
+    props.modResults = props.modResults.filter(
+      (item) => !(item.type === "property" && item.key === "org.gradle.jvmargs")
+    );
+    props.modResults.push({
+      type: "property",
+      key: "org.gradle.jvmargs",
+      value: "-Xmx4096m -XX:MaxMetaspaceSize=2048m",
+    });
+    return props;
+  });
+};
